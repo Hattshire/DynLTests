309903873e35287d904d62a89bb4e17b34e804e3
julian andrew sepulveda major <julian.sepulveda@lds-pc10.inf.santiago.usm.cl>
2016-10-07 14:39:59 -0300

    Initial Repo

---
+++ lib.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void loadThis()
+{
+	printf("%s\n", "This Works!!1!");
+}
---
+++ program_DLib.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <dlfcn.h>
+#include <string.h>
+
+
+int main() {
+	//Donde se guarda la libreria
+	void *handle;
+	//Nombre de la libreria
+	char strin[100];
+	//Funcion que se quiere obtener
+	void (*loadThis)(void);
+	
+	//Obtenemos donde esta la libreria
+	scanf("%s", strin);
+	
+	//Abrimos la libreria TODO: ¿que es RTLD_LAZY?
+	handle = dlopen (strin, RTLD_LAZY);
+	//Revisamos si la libreria se abrio correctamente
+	if (!handle) {
+		printf("error D:");
+		exit(191191);
+	}
+	
+	//Cargamos la funcion en la libreria a la funcion en este programa
+	loadThis = dlsym(handle, "loadThis");
+	//Ejecutamos la función
+	(*loadThis)();
+	
+	return 0;
+}
+
